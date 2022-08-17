6d892cca7da18ed6395cfc08256e97ee02291451
Manish Kumar <manishhansalmeh@gmail.com>
2022-08-17 16:32:24 +0530

    Stay in touch page component completed

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import CreatePost from './Components/CreatePost';
 import Footer from './Components/Footer';
+import GetInTouch from './Components/GetInTouch';
 
 function App() {
   return (
     <div>
+      <GetInTouch/>
       <Footer />
-      <CreatePost/>
+      <CreatePost />
+      
     </div>
   );
 }
