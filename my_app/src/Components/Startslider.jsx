ad535f4386599b5d269e3ef75d020f7c79a0603a
Manish Kumar <manishhansalmeh@gmail.com>
2022-08-30 18:47:09 +0530

    Update

--- my_app/src/Components/Startslider.jsx
+++ my_app/src/Components/Startslider.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import "./start.css";
 import imageFolder3 from "../imageFolder/slider1.jpeg";
 
-
-
 const Startslider = () => {
-    return (
-     
-        <>
-   
-   <div className="sidebar">
-      <a href="#" >01</a>
-      <a href="##"> 02</a>
-      <a href="###"> 03</a>
-      <a href="####"> 04</a>
-    </div>
+  return (
+    <>
+      <div className="sidebar">
+        <a href="#">01</a>
+        <a href="##"> 02</a>
+        <a href="###"> 03</a>
+        <a href="####"> 04</a>
+      </div>
 
-    
-    <div className="main_sli">
-<img className='start_img' src= {imageFolder3} alt="" classNa="img-fluid" />
-        
-    </div>
-       
-       
-       
+      <div className="main_sli">
+        <img
+          className="start_img"
+          src={imageFolder3}
+          alt=""
+          classNa="img-fluid"
+        />
+      </div>
     </>
-            )
-            }
-            export default Startslider;
+  );
+};
+export default Startslider;
